60d562e5d2dd326262a5d7232ceee27203db33a8
KarolisG <kgaivenis@gmail.com>
2024-12-15 10:44:56 +0200

    add coding problem 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const data = ["apple", "banana", "orange", "grape", "mango"];
+  const [searchQuery, setSearchQuery] = useState("");
+  const [fruitsList, setFruitsList] = useState(data);
+
+  function filterFruits(phrase) {
+    setSearchQuery(phrase);
+    let filteredList = data.filter((fruit) =>
+      fruit.toLowerCase().includes(phrase.toLowerCase())
+    );
+    phrase.length > 0 ? setFruitsList(filteredList) : setFruitsList(data);
+  }
+
+  function resetSearch() {
+    setSearchQuery("");
+    setFruitsList(data);
+  }
+
+  const FruitList = () => {
+    if (fruitsList.length === 0) {
+      return <h3>No items found</h3>;
+    }
+
+    return (
+      <ul>
+        {fruitsList.map((fruit) => {
+          return <li key={fruit}>{fruit}</li>;
+        })}
+      </ul>
+    );
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Available fruits</h1>
+
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => filterFruits(e.target.value)}
+        />
+
+        <button onClick={() => resetSearch()}>Clear</button>
+
+        <FruitList />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
